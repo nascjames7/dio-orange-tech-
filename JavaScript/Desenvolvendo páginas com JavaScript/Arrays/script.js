e6ac26592e4e0f14fce96512e7c842a5cfc2ddba
James <james.nascimento@ufrpe.br>
2022-10-28 23:26:06 -0300

    Bricando com arrays, parte 1.

--- JavaScript/Desenvolvendo páginas com JavaScript/Arrays/script.js
+++ JavaScript/Desenvolvendo páginas com JavaScript/Arrays/script.js
@@ -14,3 +14,55 @@ console.log(lista);
 //Mostra o tamanho da lista.
 console.log(lista.length);
 
+//Mostra a lista com os elementos em ordem contrária (Inverte os elemento da lista).
+console.log(lista.reverse());
+//Anula a inversão anterior.
+console.log(lista.reverse());
+
+
+//Mostra a lista com os elementos como frase (concatena, separando por vírgulas).
+console.log(lista.toString());
+
+//Imprime o primeiro elemento.
+console.log(lista[0]);
+//Imprime a prmeira letra do primeiro elemento.
+console.log(lista.toString()[0]);
+
+//Imprime os elementos da lista separados por um determinado símbolo.
+console.log(lista.join("-"));
+console.log(lista.join(" | "));
+console.log(lista.join(" # "));
+
+//Lista de objetos num array.
+var frutas = [{nome: "kiwi", cor: "verde"}, {nome: "jaca", cor: "amarelo"},
+ {nome: "amora", cor: "vinho"}];
+alert(frutas[1]. cor);
+
+//Array com tipos diferentes de variáveis.
+let tiposDistintos = [2, "que coisa", true, undefined, null, [3, 5 , 7, 9], 's', {nome: "amora", cor: "vinho"}];
+console.log(tiposDistintos);
+
+//Impressão do item e indice de cada elemento do array.
+tiposDistintos.forEach(function(item, index){console.log(item, index)});
+
+//Apaga o último elemento do array.
+tiposDistintos.pop();
+console.log(tiposDistintos.pop());
+tiposDistintos.forEach(function(item, index){console.log(item, index)});
+
+//Apaga o primeiro  elemento do array.
+tiposDistintos.shift();
+console.log(tiposDistintos.shift());
+tiposDistintos.forEach(function(item, index){console.log(item, index)});
+
+//Mostra o índice do elemento.
+console.log(tiposDistintos.indexOf(null));
+
+//Apaga os elementos entre os índices por arrays.
+tiposDistintos.splice(2,4);
+console.log(tiposDistintos);
+
+//Retorna uma parte do array.
+tiposDistintos.slice(0,3);
+console.log(tiposDistintos);
+
